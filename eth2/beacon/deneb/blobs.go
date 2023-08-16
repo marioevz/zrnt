68d785cbfc26e38dd8eba9f2c99462abd299254a
Mario Vega <marioevz@gmail.com>
2023-08-16 22:55:16 +0000

    More Deneb Types

--- eth2/beacon/deneb/blobs.go
+++ eth2/beacon/deneb/blobs.go
@@ -2,7 +2,6 @@ package deneb
 
 import (
 	"bytes"
-	"encoding/hex"
 	"errors"
 	"fmt"
 
@@ -12,38 +11,90 @@ import (
 	. "github.com/protolambda/ztyp/view"
 )
 
-const FIELD_ELEMENTS_PER_BLOB = 4096
 const BYTES_PER_FIELD_ELEMENT = 32
-const BlobSize = FIELD_ELEMENTS_PER_BLOB * BYTES_PER_FIELD_ELEMENT
 
-type Blob [BlobSize]byte
+// Eth1 deposit ordering
+type BlobIndex Uint64View
 
-var BlobType = BasicVectorType(ByteType, BlobSize)
+func AsBlobIndex(v View, err error) (BlobIndex, error) {
+	i, err := AsUint64(v, err)
+	return BlobIndex(i), err
+}
 
-func (p *Blob) Deserialize(dr *codec.DecodingReader) error {
+func (i *BlobIndex) Deserialize(dr *codec.DecodingReader) error {
+	return (*Uint64View)(i).Deserialize(dr)
+}
+
+func (i BlobIndex) Serialize(w *codec.EncodingWriter) error {
+	return w.WriteUint64(uint64(i))
+}
+
+func (BlobIndex) ByteLength() uint64 {
+	return 8
+}
+
+func (BlobIndex) FixedLength() uint64 {
+	return 8
+}
+
+func (i BlobIndex) HashTreeRoot(hFn tree.HashFn) tree.Root {
+	return Uint64View(i).HashTreeRoot(hFn)
+}
+
+func (e BlobIndex) MarshalJSON() ([]byte, error) {
+	return Uint64View(e).MarshalJSON()
+}
+
+func (e *BlobIndex) UnmarshalJSON(b []byte) error {
+	return ((*Uint64View)(e)).UnmarshalJSON(b)
+}
+
+func (e BlobIndex) String() string {
+	return Uint64View(e).String()
+}
+
+type Blob []byte
+
+func BlobSize(spec *common.Spec) uint64 {
+	return uint64(spec.FIELD_ELEMENTS_PER_BLOB) * BYTES_PER_FIELD_ELEMENT
+}
+
+func BlobType(spec *common.Spec) *BasicVectorTypeDef {
+	return BasicVectorType(ByteType, BlobSize(spec))
+}
+
+func (p *Blob) Deserialize(spec *common.Spec, dr *codec.DecodingReader) error {
 	if p == nil {
 		return errors.New("nil blob")
 	}
-	_, err := dr.Read(p[:])
-	return err
+	*p = make(Blob, BlobSize(spec))
+	n, err := dr.Read((*p)[:])
+	if err != nil {
+		return err
+	}
+	if uint64(n) != BlobSize(spec) {
+		return errors.New("incorrect number of bytes read")
+	}
+	return nil
 }
 
-func (p *Blob) Serialize(w *codec.EncodingWriter) error {
-	return w.Write(p[:])
+func (p *Blob) Serialize(spec *common.Spec, w *codec.EncodingWriter) error {
+	return w.Write((*p)[:])
 }
 
-func (Blob) ByteLength() uint64 {
-	return BlobSize
+func (Blob) ByteLength(spec *common.Spec) uint64 {
+	return spec.FIELD_ELEMENTS_PER_BLOB.ByteLength()
 }
 
-func (Blob) FixedLength() uint64 {
-	return BlobSize
+func (Blob) FixedLength(spec *common.Spec) uint64 {
+	return spec.FIELD_ELEMENTS_PER_BLOB.ByteLength()
 }
 
-func (p Blob) HashTreeRoot(hFn tree.HashFn) tree.Root {
+func (p Blob) HashTreeRoot(spec *common.Spec, hFn tree.HashFn) tree.Root {
 	return hFn.ByteVectorHTR(p[:])
 }
 
+/*
 func (p Blob) MarshalText() ([]byte, error) {
 	return []byte("0x" + hex.EncodeToString(p[:])), nil
 }
@@ -59,12 +110,13 @@ func (p *Blob) UnmarshalText(text []byte) error {
 	if len(text) >= 2 && text[0] == '0' && (text[1] == 'x' || text[1] == 'X') {
 		text = text[2:]
 	}
-	if len(text) != 2*BlobSize {
+	if len(text) != int(2*BlobSize(spec)) {
 		return fmt.Errorf("unexpected length string '%s'", string(text))
 	}
-	_, err := hex.Decode(p[:], text)
+	_, err := hex.Decode((*p)[:], text)
 	return err
 }
+*/
 
 type Blobs []Blob
 
@@ -72,18 +124,18 @@ func (li *Blobs) Deserialize(spec *common.Spec, dr *codec.DecodingReader) error 
 	return dr.List(func() codec.Deserializable {
 		i := len(*li)
 		*li = append(*li, Blob{})
-		return &((*li)[i])
-	}, BlobSize, uint64(spec.MAX_BLOBS_PER_BLOCK))
+		return spec.Wrap(&((*li)[i]))
+	}, BlobSize(spec), uint64(spec.MAX_BLOBS_PER_BLOCK))
 }
 
-func (li Blobs) Serialize(_ *common.Spec, w *codec.EncodingWriter) error {
+func (li Blobs) Serialize(spec *common.Spec, w *codec.EncodingWriter) error {
 	return w.List(func(i uint64) codec.Serializable {
-		return &li[i]
-	}, BlobSize, uint64(len(li)))
+		return spec.Wrap(&li[i])
+	}, BlobSize(spec), uint64(len(li)))
 }
 
-func (li Blobs) ByteLength(_ *common.Spec) (out uint64) {
-	return BlobSize * uint64(len(li))
+func (li Blobs) ByteLength(spec *common.Spec) (out uint64) {
+	return BlobSize(spec) * uint64(len(li))
 }
 
 func (*Blobs) FixedLength(*common.Spec) uint64 {
@@ -94,14 +146,14 @@ func (li Blobs) HashTreeRoot(spec *common.Spec, hFn tree.HashFn) common.Root {
 	length := uint64(len(li))
 	return hFn.ComplexListHTR(func(i uint64) tree.HTR {
 		if i < length {
-			return &li[i]
+			return spec.Wrap(&li[i])
 		}
 		return nil
 	}, length, uint64(spec.MAX_BLOBS_PER_BLOCK))
 }
 
 func BlobsType(spec *common.Spec) *ComplexListTypeDef {
-	return ComplexListType(BlobType, uint64(spec.MAX_BLOBS_PER_BLOCK))
+	return ComplexListType(BlobType(spec), uint64(spec.MAX_BLOBS_PER_BLOCK))
 }
 
 func (li Blobs) View(spec *common.Spec) (*BlobsView, error) {
@@ -129,17 +181,168 @@ func (v *BlobsView) Raw(spec *common.Spec) (*Blobs, error) {
 	if err := v.Serialize(codec.NewEncodingWriter(&buf)); err != nil {
 		return nil, err
 	}
-	if buf.Len()%BlobSize != 0 {
+	blobSize := int(BlobSize(spec))
+	if buf.Len()%blobSize != 0 {
 		return nil, fmt.Errorf("invalid length for blobs: %d", buf.Len())
 	}
-	commitmentCount := buf.Len() / BlobSize
+	commitmentCount := buf.Len() / blobSize
 	if commitmentCount > int(spec.MAX_BLOBS_PER_BLOCK) {
 		return nil, fmt.Errorf("too many Blobs: %d", commitmentCount)
 	}
 	bufBytes := buf.Bytes()
-	out := make(Blobs, len(bufBytes)/BlobSize)
-	for c := 0; c < len(bufBytes)/BlobSize; c++ {
-		copy(out[c][:], bufBytes[c*BlobSize:(c+1)*BlobSize])
+	out := make(Blobs, len(bufBytes)/blobSize)
+	for c := 0; c < len(bufBytes)/blobSize; c++ {
+		copy(out[c][:], bufBytes[c*blobSize:(c+1)*blobSize])
 	}
 	return &out, nil
+}
+
+func BlobRootsType(spec *common.Spec) ListTypeDef {
+	return ListType(RootType, uint64(spec.MAX_BLOBS_PER_BLOCK))
+}
+
+type BlobRoots []tree.Root
+
+func (br *BlobRoots) Deserialize(spec *common.Spec, dr *codec.DecodingReader) error {
+	return dr.List(func() codec.Deserializable {
+		i := len(*br)
+		*br = append(*br, tree.Root{})
+		return &(*br)[i]
+	}, RootType.TypeByteLength(), uint64(spec.MAX_BLOBS_PER_BLOCK))
+}
+
+func (br BlobRoots) Serialize(spec *common.Spec, w *codec.EncodingWriter) error {
+	return w.List(func(i uint64) codec.Serializable {
+		return &br[i]
+	}, RootType.TypeByteLength(), uint64(len(br)))
+}
+
+func (br BlobRoots) ByteLength(spec *common.Spec) uint64 {
+	return uint64(len(br)) * RootType.TypeByteLength()
+}
+
+func (br BlobRoots) FixedLength(spec *common.Spec) uint64 {
+	return 0 // it's a list, no fixed length
+}
+
+func (br BlobRoots) HashTreeRoot(spec *common.Spec, hFn tree.HashFn) tree.Root {
+	return hFn.ChunksHTR(func(i uint64) tree.Root {
+		return br[i]
+	}, uint64(len(br)), uint64(spec.MAX_BLOBS_PER_BLOCK))
+}
+
+func BlobSidecarType(spec *common.Spec) *ContainerTypeDef {
+	return ContainerType("BlobSidecar", []FieldDef{
+		{"block_root", RootType},
+		{"index", Uint64Type},
+		{"slot", common.SlotType},
+		{"block_parent_root", RootType},
+		{"proposer_index", common.ValidatorIndexType},
+		{"blob", BlobType(spec)},
+		{"kzg_commitment", common.KZGCommitmentType},
+		{"kzg_proof", common.KZGProofType},
+	})
+}
+
+type BlobSidecar struct {
+	BlockRoot       common.Root           `json:"block_root" yaml:"block_root"`
+	Index           BlobIndex             `json:"index" yaml:"index"`
+	Slot            common.Slot           `json:"slot" yaml:"slot"`
+	BlockParentRoot common.Root           `json:"block_parent_root" yaml:"block_parent_root"`
+	ProposerIndex   common.ValidatorIndex `json:"proposer_index" yaml:"proposer_index"`
+	Blob            Blob                  `json:"blob" yaml:"blob"`
+	KZGCommitment   common.KZGCommitment  `json:"kzg_commitment" yaml:"kzg_commitment"`
+	KZGProof        common.KZGProof       `json:"kzg_proof" yaml:"kzg_proof"`
+}
+
+func (b *BlobSidecar) Deserialize(spec *common.Spec, dr *codec.DecodingReader) error {
+	return dr.Container(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, spec.Wrap(&b.Blob), &b.KZGCommitment, &b.KZGProof)
+}
+
+func (b *BlobSidecar) Serialize(spec *common.Spec, w *codec.EncodingWriter) error {
+	return w.Container(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, spec.Wrap(&b.Blob), &b.KZGCommitment, &b.KZGProof)
+}
+
+func (b *BlobSidecar) ByteLength(spec *common.Spec) uint64 {
+	return codec.ContainerLength(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, spec.Wrap(&b.Blob), &b.KZGCommitment, &b.KZGProof)
+}
+
+func (b *BlobSidecar) FixedLength(spec *common.Spec) uint64 {
+	return codec.ContainerLength(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, spec.Wrap(&b.Blob), &b.KZGCommitment, &b.KZGProof)
+}
+
+func (b *BlobSidecar) HashTreeRoot(spec *common.Spec, hFn tree.HashFn) common.Root {
+	return hFn.HashTreeRoot(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, spec.Wrap(&b.Blob), &b.KZGCommitment, &b.KZGProof)
+}
+
+var BlindedBlobSidecarType = ContainerType("BlindedBlobSidecar", []FieldDef{
+	{"block_root", RootType},
+	{"index", Uint64Type},
+	{"slot", common.SlotType},
+	{"block_parent_root", RootType},
+	{"proposer_index", common.ValidatorIndexType},
+	{"blob_root", RootType},
+	{"kzg_commitment", common.KZGCommitmentType},
+	{"kzg_proof", common.KZGProofType},
+})
+
+type BlindedBlobSidecar struct {
+	BlockRoot       common.Root           `json:"block_root" yaml:"block_root"`
+	Index           BlobIndex             `json:"index" yaml:"index"`
+	Slot            common.Slot           `json:"slot" yaml:"slot"`
+	BlockParentRoot common.Root           `json:"block_parent_root" yaml:"block_parent_root"`
+	ProposerIndex   common.ValidatorIndex `json:"proposer_index" yaml:"proposer_index"`
+	BlobRoot        common.Root           `json:"blob_root" yaml:"blob_root"`
+	KZGCommitment   common.KZGCommitment  `json:"kzg_commitment" yaml:"kzg_commitment"`
+	KZGProof        common.KZGProof       `json:"kzg_proof" yaml:"kzg_proof"`
+}
+
+func (b *BlindedBlobSidecar) Deserialize(dr *codec.DecodingReader) error {
+	return dr.Container(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, &b.BlobRoot, &b.KZGCommitment, &b.KZGProof)
+}
+
+func (b *BlindedBlobSidecar) Serialize(w *codec.EncodingWriter) error {
+	return w.Container(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, &b.BlobRoot, &b.KZGCommitment, &b.KZGProof)
+}
+
+func (b *BlindedBlobSidecar) ByteLength() uint64 {
+	return codec.ContainerLength(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, &b.BlobRoot, &b.KZGCommitment, &b.KZGProof)
+}
+
+func (b *BlindedBlobSidecar) FixedLength() uint64 {
+	return codec.ContainerLength(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, &b.BlobRoot, &b.KZGCommitment, &b.KZGProof)
+}
+
+func (b *BlindedBlobSidecar) HashTreeRoot(hFn tree.HashFn) common.Root {
+	return hFn.HashTreeRoot(&b.BlockRoot, &b.Index, &b.Slot, &b.BlockParentRoot, &b.ProposerIndex, &b.BlobRoot, &b.KZGCommitment, &b.KZGProof)
+}
+
+var SignedBlindedBlobSidecarType = ContainerType("SignedBlindedBlobSidecar", []FieldDef{
+	{"message", BlindedBlobSidecarType},
+	{"signature", common.BLSSignatureType},
+})
+
+type SignedBlindedBlobSidecar struct {
+	Message   BlindedBlobSidecar  `json:"message" yaml:"message"`
+	Signature common.BLSSignature `json:"signature" yaml:"signature"`
+}
+
+func (b *SignedBlindedBlobSidecar) Deserialize(dr *codec.DecodingReader) error {
+	return dr.Container(&b.Message, &b.Signature)
+}
+
+func (b *SignedBlindedBlobSidecar) Serialize(w *codec.EncodingWriter) error {
+	return w.Container(&b.Message, &b.Signature)
+}
+
+func (b *SignedBlindedBlobSidecar) ByteLength() uint64 {
+	return codec.ContainerLength(&b.Message, &b.Signature)
+}
+
+func (b *SignedBlindedBlobSidecar) FixedLength() uint64 {
+	return codec.ContainerLength(&b.Message, &b.Signature)
+}
+
+func (b *SignedBlindedBlobSidecar) HashTreeRoot(hFn tree.HashFn) common.Root {
+	return hFn.HashTreeRoot(&b.Message, &b.Signature)
 }
--- eth2/beacon/deneb/blobs_bundle.go
+++ eth2/beacon/deneb/blobs_bundle.go
@@ -9,8 +9,8 @@ import (
 
 func BlobsBundleType(spec *common.Spec) *ContainerTypeDef {
 	return ContainerType("BlobsBundle", []FieldDef{
-		{"kzg_commitments", common.KZGCommitmentsType(spec)},
-		{"kzg_proofs", common.KZGProofsType(spec)},
+		{"commitments", common.KZGCommitmentsType(spec)},
+		{"proofs", common.KZGProofsType(spec)},
 		{"blobs", BlobsType(spec)},
 	})
 }
@@ -48,4 +48,38 @@ func (a *BlobsBundle) FixedLength(*common.Spec) uint64 {
 
 func (s *BlobsBundle) HashTreeRoot(spec *common.Spec, hFn tree.HashFn) common.Root {
 	return hFn.HashTreeRoot(spec.Wrap(&s.KZGCommitments), spec.Wrap(&s.KZGProofs), spec.Wrap(&s.Blobs))
+}
+
+func BlindedBlobsBundleType(spec *common.Spec) *ContainerTypeDef {
+	return ContainerType("BlindedBlobsBundle", []FieldDef{
+		{"commitments", common.KZGCommitmentsType(spec)},
+		{"proofs", common.KZGProofsType(spec)},
+		{"blob_roots", BlobRootsType(spec)},
+	})
+}
+
+type BlindedBlobsBundle struct {
+	KZGCommitments common.KZGCommitments
+	KZGProofs      common.KZGProofs
+	BlobRoots      BlobRoots
+}
+
+func (b *BlindedBlobsBundle) Deserialize(spec *common.Spec, dr *codec.DecodingReader) error {
+	return dr.Container(spec.Wrap(&b.KZGCommitments), spec.Wrap(&b.KZGProofs), spec.Wrap(&b.BlobRoots))
+}
+
+func (b *BlindedBlobsBundle) Serialize(spec *common.Spec, w *codec.EncodingWriter) error {
+	return w.Container(spec.Wrap(&b.KZGCommitments), spec.Wrap(&b.KZGProofs), spec.Wrap(&b.BlobRoots))
+}
+
+func (b *BlindedBlobsBundle) ByteLength(spec *common.Spec) uint64 {
+	return codec.ContainerLength(spec.Wrap(&b.KZGCommitments), spec.Wrap(&b.KZGProofs), spec.Wrap(&b.BlobRoots))
+}
+
+func (a *BlindedBlobsBundle) FixedLength(*common.Spec) uint64 {
+	return 0
+}
+
+func (b *BlindedBlobsBundle) HashTreeRoot(spec *common.Spec, hFn tree.HashFn) common.Root {
+	return hFn.HashTreeRoot(spec.Wrap(&b.KZGCommitments), spec.Wrap(&b.KZGProofs), spec.Wrap(&b.BlobRoots))
 }
